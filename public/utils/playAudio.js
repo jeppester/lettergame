6497d53afd1632d8a1a4fc80cd83215326ac65b8
Jesper Sørensen <jeppester1@gmail.com>
2023-07-31 22:14:54 +0200

    PlayAudio: Make cancelable and add volume parameter

--- public/utils/playAudio.js
+++ public/utils/playAudio.js
@@ -1,9 +1,39 @@
-export default function playAudio(context, audioBuffer) {
-  return new Promise((resolve) => {
-    const source = context.createBufferSource()
-    source.buffer = audioBuffer
+export default function playAudio(context, audioBuffer, volume = 1, loop = false) {
+  let gainNode
+
+  const source = context.createBufferSource()
+  if (loop) source.loop = true
+
+  source.buffer = audioBuffer
+
+  if (volume !== 1) {
+    gainNode = context.createGain()
+    gainNode.gain.value = volume
+    source.connect(gainNode)
+    gainNode.connect(context.destination)
+  }
+  else {
     source.connect(context.destination)
-    source.addEventListener('ended', resolve)
+  }
+
+  const endPlayback = () => {
+    source.stop()
+    if (gainNode) {
+      gainNode.disconnect()
+    }
+    source.disconnect()
+  }
+
+  const promise = new Promise((resolve) => {
+    source.addEventListener('ended', () => {
+      endPlayback()
+      resolve()
+    })
+
     source.start(0)
   })
+
+  promise.cancel = endPlayback
+
+  return promise
 }
